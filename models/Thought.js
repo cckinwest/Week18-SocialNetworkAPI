2cac89cce6359786c415f72e1da4732f5c6e6570
cckinwest <kensechan@gmail.com>
2023-08-12 20:36:48 +0100

    date reformat works now

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,15 @@
 const { Schema, model, Types } = require("mongoose");
 
+function reformat(dateObj) {
+  const dateString = dateObj.toISOString();
+
+  const date = dateString.split("T")[0];
+  const hour = dateString.split("T")[1].split(":")[0];
+  const minute = dateString.split("T")[1].split(":")[1];
+
+  return `On ${date}, at ${hour}:${minute}`;
+}
+
 const reactionSchema = new Schema(
   {
     reactionId: {
@@ -17,7 +27,8 @@ const reactionSchema = new Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now(),
+      default: new Date(),
+      get: (v) => reformat(v),
     },
   },
   {
@@ -32,11 +43,12 @@ const thoughtSchema = new Schema(
       type: String,
       required: [true, "Text required!"],
       minlength: [1, "Text required!"],
-      maxlength: [280, "Text length exceeds 280!"],
+      maxlength: [280, "Text length (exceeds 280!"],
     },
     createdAt: {
       type: Date,
-      default: Date.now,
+      default: new Date(),
+      get: (v) => reformat(v),
     },
     username: {
       type: String,
@@ -45,7 +57,7 @@ const thoughtSchema = new Schema(
     reactions: [reactionSchema],
   },
   {
-    toJSON: { virtuals: true },
+    toJSON: { virtuals: true, getters: true },
     id: false,
   }
 );
--- models/User.js
+++ models/User.js
@@ -1,39 +1,49 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "Username required!"],
-    unique: [true, "This username is used by someone else."],
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    validate: {
-      validator: function (v) {
-        const emailRegex = new RegExp(
-          /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm
-        );
-        return emailRegex.test(v);
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username required!"],
+      unique: [true, "This username is used by someone else."],
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          const emailRegex = new RegExp(
+            /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm
+          );
+          return emailRegex.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
       },
-      message: (props) => `${props.value} is not a valid email!`,
+      required: [true, "Email required!"],
     },
-    required: [true, "Email required!"],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
   },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "thought",
-    },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-  ],
+  {
+    toJSON: { virtuals: true },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
 const User = model("user", userSchema);
